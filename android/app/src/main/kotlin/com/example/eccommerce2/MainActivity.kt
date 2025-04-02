81bc0a52b6484f7fe0db9475599d37c04a6ba060
ibrahimshkhaleel <ebrahim_shaban16@yahoo.com>
2025-04-02 06:37:03 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/eccommerce2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.eccommerce2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/view/widgets/home/card_items.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../logic/controllers/cart_controller.dart';
+import '../../../logic/controllers/product_controller.dart';
+import '../../../models/product_models.dart';
+import '../../../utils/theme.dart';
+import '../../screens/product_details_scree.dart';
+import '../text_utils.dart';
+
+
+class CardItems extends StatelessWidget {
+  CardItems({Key? key}) : super(key: key);
+
+  final controller = Get.find<ProductController>();
+
+  final cartController = Get.find<CartController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      if (controller.isLoading.value) {
+        return Center(
+          child: CircularProgressIndicator(
+            color: Get.isDarkMode ? pinkClr : mainColor,
+          ),
+        );
+      } else {
+        return Expanded(
+          child: controller.searchList.isEmpty &&
+                  controller.searchTextController.text.isNotEmpty
+              ? Get.isDarkMode
+                  ? Image.asset("assets/images/search_empty_dark.png")
+                  : Image.asset("assets/images/search_empry_light.png")
+              : GridView.builder(
+                  itemCount: controller.searchList.isEmpty
+                      ? controller.productList.length
+                      : controller.searchList.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    childAspectRatio: 0.8,
+                    mainAxisSpacing: 9.0,
+                    crossAxisSpacing: 6.0,
+                    maxCrossAxisExtent: 200,
+                  ),
+                  itemBuilder: (context, index) {
+                    if (controller.searchList.isEmpty) {
+                      return buildCardItems(
+                          image: controller.productList[index].image,
+                          title: controller.productList[index].title,
+                          price: controller.productList[index].price,
+                          rate: controller.productList[index].rating.rate,
+                          productId: controller.productList[index].id,
+                          productModels: controller.productList[index],
+                          onTap: () {
+                            Get.to(() => ProductDetailsScreen(
+                                  productModels: controller.productList[index],
+                                ));
+                          });
+                    } else {
+                      return buildCardItems(
+                          image: controller.searchList[index].image,
+                          title: controller.searchList[index].title,
+                          price: controller.searchList[index].price,
+                          rate: controller.searchList[index].rating.rate,
+                          productId: controller.searchList[index].id,
+                          productModels: controller.searchList[index],
+                          onTap: () {
+                            Get.to(() => ProductDetailsScreen(
+                                  productModels: controller.searchList[index],
+                                ));
+                          });
+                    }
+                  },
+                ),
+        );
+      }
+    });
+  }
+
+  Widget buildCardItems({
+    required String image,
+    required String title,
+    required double price,
+    required double rate,
+    required int productId,
+    required ProductModels productModels,
+    required Function() onTap,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 3.0,
+                blurRadius: 5.0,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Obx(
+                () => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        controller.manageFavourites(productId);
+                      },
+                      icon: controller.isFavourites(productId)
+                          ? const Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                            )
+                          : const Icon(
+                              Icons.favorite_outline,
+                              color: Colors.black,
+                            ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        cartController.addProductToCart(productModels);
+                      },
+                      icon: const Icon(
+                        Icons.shopping_cart,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 2,),
+              Container(
+                width: double.infinity,
+                height: 90,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Image.network(
+                  image,
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15,),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "\$ $price",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 11,
+                      ),
+                    ),
+
+
+                    Container(
+                      height: 20,
+                      width: 45,
+                      decoration: BoxDecoration(
+                        color: mainColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 3, right: 2),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextUtils(
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                              text: "$rate",
+                              color: Colors.white,
+                              underLine: TextDecoration.none,
+                            ),
+                            const Icon(
+                              Icons.star,
+                              size: 13,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+
+                      ),
+
+                    ),
+
+                  ],
+                ),
+              ),
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 11,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
